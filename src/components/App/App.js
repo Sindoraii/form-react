5f16eb1980b636b37c26aff8f9e4691b7b6f3ea5
Mariya Krupenina <marise.sindorai@yandex.ru>
2023-01-17 13:29:31 +0300

    init: init react app

---
+++ src/components/App/App.js
@@ -0,0 +1,7 @@
+const App = ({entity,isEdit}) => {
+  return (
+    <div className="form"></div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './components/App/App';
+
+const user = {
+    name: 'Name',
+    surname: 'Surname',
+    email: 'emailsdsk@gmail.com',
+    dateOfBirth: '1994-02-13',
+    cardNumber: '7777 7777 7777 7777',
+    cardExpiration: '2023-02',
+    cardCvc: '123',
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App entity={user} isEdit={false}/>
+  </React.StrictMode>
+);
